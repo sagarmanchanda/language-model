577ef6c5490db44babc34e2b68661b17026ec93b
Sagar Manchanda <sagarmanchanda.iitg@gmail.com>
2016-06-24 16:31:14 +0530

    Removed test file.

--- try.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import rnn
-
-X_train = np.load('saved-states/X_train.npy')
-y_train = np.load('saved-states/y_train.npy')
-
-model = rnn.RNN(8000)
-print model.calculateLoss(X_train[:1000], y_train[:1000])
